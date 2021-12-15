fe1d3086b440a5d38202f31519014e0bdfc1f118
AL3X-_- <avasilega10@gmail.com>
2021-12-15 13:32:12 +0300

    Silent mode enabled for curl

--- Detector/get_helper_functions.sh
+++ Detector/get_helper_functions.sh
@@ -4,5 +4,5 @@ files=("utils.py" "transforms.py" "coco_eval.py" "engine.py" "coco_utils.py")
 
 for file in ${files[*]}
 do
-	curl https://raw.githubusercontent.com/pytorch/vision/main/references/detection/${file}  --output ${file}
+	curl https://raw.githubusercontent.com/pytorch/vision/main/references/detection/${file} -s --output ${file}
 done
